9ae3c10860333fbeca78e957d063e6fa83713be5
FractalIceCream <vroco86@gmail.com>
2023-09-28 01:34:03 -0500

    added logic.js

---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+var questions = [
+    {
+        q: "Which of the following is not a primitive data type in JavaScript?",
+        choices: ["Number", "String", "Boolean", "Object"],
+        correct: "Object"
+    },
+    {
+        q: "What does the \"typeof\" operator do in JavaScript?",
+        choices: ["Returns the data type of a variable",
+            "Checks if a variable is defined",
+            "Assigns a value to a variable",
+            "Concatenation two strings"],
+        correct: "Returns the data type of a variable"
+    }
+];
+
